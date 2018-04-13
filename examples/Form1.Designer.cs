1edb38bf01d12293351eb16899865dd085f162ec
Adnan Kamili <adnan.kamili@gmail.com>
2018-04-13 18:22:50 +0530

    updated form

--- examples/Form1.Designer.cs
+++ examples/Form1.Designer.cs
@@ -42,7 +42,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(68, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Product Key:";
+            this.label1.Text = "License Key:";
             // 
             // productKeyBox
             // 
