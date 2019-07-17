744fceec074bdebff367d87dba599d1510c62aec
Adnan Kamili <adnan.kamili@gmail.com>
2019-07-17 17:27:31 +0530

    updated version

--- pre-build.ps1
+++ pre-build.ps1
@@ -1,14 +1,8 @@
 $lexactivator_version = "v3.7.1"
 $url = "https://dl.cryptlex.com/downloads/$lexactivator_version/LexActivator-Win.zip"
 $output = "$PSScriptRoot\LexActivator-Win.zip"
-$start_time = Get-Date
 
-$wc = New-Object System.Net.WebClient
-$wc.DownloadFile($url, $output)
-#OR
 (New-Object System.Net.WebClient).DownloadFile($url, $output)
-
-Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
 
 Expand-Archive "$PSScriptRoot\LexActivator-Win.zip" -DestinationPath "$PSScriptRoot\LexActivator-Win"
 
